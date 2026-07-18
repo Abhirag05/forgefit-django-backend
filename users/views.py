083ce9d58b0faf36b3957642d5cf257c09cfe093
Abhirag.S.V <abhiragsv2005@gmail.com>
2026-07-18 21:07:33 +0530

    feat: update index template and user view to utilize dynamic context variables

--- users/views.py
+++ users/views.py
@@ -2,5 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+class User:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
 def home(request):
-    return render(request,'index.html')
+
+    context={
+        'user':User('Abhi',21),
+        'name':'Abhirag S V',
+        'age' :20,
+        'skills':['python','django','javascript'],
+        'address':{
+            'title':"My details",
+            'city':'Bangalore',
+            'state':'Karnataka',
+        },
+        'empty_value':None,
+    }
+    return render(request,'index.html',context)
